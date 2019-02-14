85a19eb61cc775b383c22070a87ade55b522ed72
Dylan Clements <dclements@protonmail.com>
2019-02-14 15:16:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,59 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @pets = {fishes: [], dogs: [], cats: []}
+    @@all << self
+  end
+    #Class Methods
+  def self.all
+    @@all
+  end
+  def self.count
+    self.all.count
+  end
+  def self.reset_all
+    self.all.clear
+  end
+  ## Instance Methods
+  def say_species
+    "I am a #{@species}."
+  end
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+  def walk_dogs
+    @pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+  def feed_fish
+    @pets[:fishes].each {|fish| fish.mood = "happy"}
+  end
+  def sell_pets
+    @pets.each do |type, owners_pets|
+      owners_pets.each do |pet|
+        pet.mood =  "nervous"
+      end
+    end
+    @pets.clear
+  end
+  def list_pets
+
+  end
 end
